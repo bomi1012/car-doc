747bda730bfa8af0c1ca5f8dbcfdc9d362aa58b2
bomi1012 <bomi1012@hs-karlsruhe.de>
2014-11-22 14:27:43 +0100

    map richtige location 2

--- custom/js/js_mb.js
+++ custom/js/js_mb.js
@@ -1,9 +1,22 @@
-var location =  [49.13354 , 8.14299];
-$('#map').initMap({ 
+/**
+ * @description Documentation: http://wakooka.github.io/initmapjs/documentation.html
+ */
+var location_latlng = [49.13354 , 8.14299];
+var address = "Große Ahlmühle 11, 76865 Rohrbach";
+ 
+$('#map').initMap({     
     // Set the center of the map to Paris
-    center: location,
+    center: location_latlng,
     markers : {
-        paris_marker: { position: location }
+        paris_marker: { 
+            position: location_latlng ,
+            info_window : { 
+                content : address,
+                showOn: 'mouseover',
+                hideOn: 'mouseout',
+                maxWidth: 150,
+                zIndex: 2
+            } }
     },
     options: {        
         zoom: 15,
