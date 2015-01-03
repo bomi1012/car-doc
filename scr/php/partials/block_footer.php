9a0b981fd0c8ff480c7b5d92ab133a0a7394fe11
bomi1012 <bomi1012@hs-karlsruhe.de>
2015-01-03 17:18:04 +0100

    10h und footer

--- scr/php/partials/block_footer.php
+++ scr/php/partials/block_footer.php
@@ -5,8 +5,84 @@
                 2014 © Car-Doc GmbH 
             </div>
             <div class="col-md-7 col-sm-12">
-                <div class="right_mb md-trigger cursor_pointer_mb" data-modal="modal-1"> Impressum</div>
+                <div class="right_mb md-trigger cursor_pointer_mb" data-modal="modal-1"> Impressum </div>
+                <div class="right_mb md-trigger cursor_pointer_mb" data-modal="modal-2"> Datenschutzerklärung </div>
+            </div>
 
+            <div class="md-modal" id="modal-2">
+                <div class="md-content">
+                    <div class="icon-close md-close"></div>
+                    <div class="container">
+                        <div class="body">
+                            <h1 class="title">Datenschutzerklärung</h1> 
+                            <h2>Datenschutzerklärung</h2>
+                            <p>Nachfolgend möchten wir Sie über den Umgang mit Ihren personenbezogenen Daten informieren. 
+                                Wir halten uns an die deutschen Datenschutzgesetze nach dem TMG und dem BDSG.</p>
+
+                            <h2>Erhebung, Verarbeitung und Nutzung personenbezogener Daten</h2>
+                            <p>Sie können unsere Seite besuchen, ohne Angaben zu Ihrer Person zu machen. 
+                                Wir speichern lediglich Zugriffsdaten ohne Personenbezug wie z.B. den Namen Ihres 
+                                Internetanbieters, die Webseite, von der aus Sie uns besuchen oder den Namen der angeforderten Datei. 
+                                Diese Daten werden ausschließlich zur Verbesserung unseres Angebotes ausgewertet und erlauben 
+                                keinen Rückschluss auf Ihre Person. Diese Daten werden von uns nicht an Dritte weitergegeben.</p>
+
+                            <p>Personenbezogene Daten werden nur erhoben, wenn Sie uns diese im Rahmen Ihrer Kontaktaufnahme.
+                                Wir verwenden die von Ihnen mitgeteilten Daten ohne Ihre gesonderte Einwilligung ausschließlich zur
+                                Erfüllung und Abwicklung Ihrer Anfrage. Mit vollständiger Abwicklung der Anfrage werden Ihre Daten für 
+                                die weitere Verwendung gesperrt und nach Ablauf der steuer- und handelsrechtlichen Aufbewahrungsfristen
+                                gelöscht.</p>
+
+                            <h2> Google Analytics </h2>
+                            <p>Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. ("Google"). 
+                                Google Analytics verwendet sog. "Cookies", Textdateien, die auf Ihrem Computer gespeichert werden und die 
+                                eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen 
+                                über Ihre Benutzung dieser Website (ausschließlich Ihrer IP-Adresse) wird an einen Server von Google 
+                                in den USA übertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre 
+                                Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten für die Websitebetreiber 
+                                zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene 
+                                Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, 
+                                sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. 
+                                Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten von Google in Verbindung bringen. 
+                                Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software 
+                                verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche 
+                                Funktionen dieser Website vollumfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie 
+                                sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und 
+                                Weise und zu dem zuvor benannten Zweck einverstanden. </p>
+                            <p>Sie können ein Browser-Addon herunterladen, um Google Analytics auf dem jeweiligen Rechner auf 
+                                dem das Addon installiert ist, zu deaktivieren. Bitte beachten Sie die weiteren Hinweise von Google 
+                                auf folgender Webseite auf der Sie das Addon auch herunterladen können:</p>
+                            <p>https://tools.google.com/dlpage/gaoptout?hl=de </p>
+
+                            <h2> Weitergabe personenbezogener Daten </h2> 
+                            <p>Eine Weitergabe Ihrer Daten erfolgt an das mit der Lieferung beauftragte Versandunternehmen, 
+                                soweit dies zur Lieferung der Waren notwendig ist. Zur Abwicklung von Zahlungen geben wir Ihre 
+                                Zahlungsdaten an das mit der Zahlung beauftragte Kreditinstitut weiter.</p>
+                            <h2>Datensicherheit</h2>
+                            <p>Nachdem Sie sich mit Ihren Zugangsdaten auf unserer Webseite eingeloggt haben,
+                                wird die weitere Kommunikation und Übermittlung von Daten verschlüsselt. 
+                                Insbesondere sämtliche Daten zur Zahlung werden zwischen Ihnen und unserer Webseite 
+                                verschlüsselt ausgetauscht. Bitte beachten Sie, dass ein Zugriff Dritter hierdurch nicht zu 
+                                einhundert Prozent ausgeschlossen werden kann. Zu ihrer eigenen Sicherheit empfehlen wir Ihnen, 
+                                sich bei Verlassen der Webseite zuvor auszuloggen und das Browserfenster zu schließen. 
+                                Bitte stellen Sie auch sicher, dass Dritte keine Kenntnis von Ihren Zugangsdaten erhalten.</p>
+                            <h2>Auskunftsrecht</h2>
+                            <p>Nach dem Bundesdatenschutzgesetz haben Sie ein Recht auf unentgeltliche 
+                                Auskunft über Ihre gespeicherten Daten sowie ggf. ein Recht auf Berichtigung, 
+                                Sperrung oder Löschung dieser Daten.</p>
+
+                            <h2>Ansprechpartner für Datenschutz </h2>
+                            <p>Bei Fragen zur Erhebung, Verarbeitung oder Nutzung Ihrer personenbezogenen Daten, bei Auskünften,
+                                Berichtigung, Sperrung oder Löschung von Daten sowie Widerruf erteilter Einwilligungen 
+                                wenden Sie sich bitte an: </p>
+
+                            <p>Eugen Luppow <br/>
+                                Pappelweg 8 <br/>
+                                77767 Appenweier <br/>
+                                info@car-doc.info <br/>
+                                06349 9964901 </p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="md-modal" id="modal-1">
@@ -29,6 +105,8 @@
                             <p>Registergericht: Amtsgericht Freiburg i. Br.<br/>
                                 Registemummer: HRB 706556<br/>
                                 Umsatzsteuer-Identiﬁkationsnmnmer: DE276l57141</p>
+                            <p>Verantwortlich nach § 55 Rundfunkstaatsvertrag <br/>
+                                Herr Eugen Luppow, CarDoc GmbH, Pappelweg 8, 77767 Appenweier. </p>
 
                             <h3>1. Haftungsbeschränkung</h3>
                             <p>Die Inhalte dieser Website werden mit größtmöglicher Sorgfalt erstellt. Der Anbieter
