1fd021b2ebd8e5fbf2636966bca94473d855fe2b
bomi1012 <bomi1012@hs-karlsruhe.de>
2015-04-15 21:01:31 +0200

    mail muss jetzt funktionieren

--- scr/php/mailer.php
+++ scr/php/mailer.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "e.luppow@car-doc.info";
+        $recipient = IConstants::CONTACT_EMAIL;
 
         // Set the email subject.
         $subject = "car-doc | new contact from $name";
